563f88c0127a5bfcee0b279daebaecf2f719e650
IvanGorbache <Ivan.Gorbache@msmail.ariel.ac.il>
2026-04-06 18:22:27 +0300

    solution

--- oneliners.py
+++ oneliners.py
@@ -8,40 +8,40 @@ if __name__ == '__main__':
            output[i] = d;
         }
     """
-    n,d = map(int, input().split())
-    output = "" # Put your line here (my solution: 5 chars)
+    n, d = map(int, input().split())
+    output = [d] * n  # Put your line here (my solution: 5 chars)
     print(output)
 
     """
     B. Given a list v, construct a vector of n-1 differences between adjacent cells.
        Example: v=[1,4,9,16], output=[3,5,7]
     """
-    v=eval(input())
-    output = "" # Put your line here (my solution: 23 chars)
+    v = eval(input())
+    output = [y - x for x, y in zip(v, v[1:])]  # Put your line here (my solution: 23 chars)
     print(output)
-    
+
     """
     C. Given a list v and a number n, find *all* indices that contain the given number.
        Example: v=[1,4,9,16,9,4], n=9, output=[2,4], since 9 is found in indices 2 and 4.
     """
-    v,n = [f(x) for f,x in zip((eval,int),input().split())]
-    output = "" # Put your line here (my solution: 37 chars)
+    v, n = [f(x) for f, x in zip((eval, int), input().split())]
+    output = [x for x, y in enumerate(v) if y == n]  # Put your line here (my solution: 37 chars)
     print(output)
 
     """
     D. Given a list v and a number n, return a string where each number is replaced with "=" if it equals n and "!" if not.
        Example: v=[1,4,9,16,9,4], n=9, output="!!=!=!"
     """
-    v,n = [f(x) for f,x in zip((eval,int),input().split())]
-    output = "" # Put your line here (my solution: 42 chars)
+    v, n = [f(x) for f, x in zip((eval, int), input().split())]
+    output = ''.join("=" if x == n else "!" for x in v)  # Put your line here (my solution: 42 chars)
     print(output)
 
     """
     E. Given a list v, return another list in which each element in the original list appears only once, in increasing order.
        Example: v=[16,4,9,16,9,4,1], output=[1,4,9,16]
     """
-    v=eval(input())
-    output = "" # Put your line here (my solution: 14 chars).
+    v = eval(input())
+    output = sorted(set(v))  # Put your line here (my solution: 14 chars).
     print(output)
 
     """
@@ -49,23 +49,24 @@ if __name__ == '__main__':
        Round the outcome down to the nearest integer.
        Example: u=[1,2,3,4,5], v=[1,2,0,0,5], output=5 (= the sqrt of (0^2+0^2+3^2+4^2+0^2))
     """
-    u,v = map(eval, input().split())
-    from math import sqrt 
-    output = "" # Put your line here (my solution: 40 chars)
+    u, v = map(eval, input().split())
+    from math import sqrt
+
+    output = int(sqrt(sum((x - y) ** 2 for x, y in zip(u, v))))  # Put your line here (my solution: 40 chars)
     print(output)
 
     """
     G. Given a matrix m (a list of two or more lists of the same length), return a single 1-dimensional list that is the concatenation of all rows. 
        Example: m=[[1,2],[3,4],[5,6]], output=[1,2,3,4,5,6]
     """
-    m=eval(input())
-    output = "" # Put your line here (my solution: 9 chars)
+    m = eval(input())
+    output = sum(m, [])  # Put your line here (my solution: 9 chars)
     print(output)
 
     """
     H. Given a matrix m, return its transpose. 
        Example: m=[[1,2],[3,4],[5,6]], output=[[1,3,5],[2,4,6]]
     """
-    m=eval(input())
-    output = ""   # Put your line here (my solution: 24 chars. Hint: *m)
+    m = eval(input())
+    output = [list(x) for x in zip(*m)]  # Put your line here (my solution: 24 chars. Hint: *m)
     print(output)
